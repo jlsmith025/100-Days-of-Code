4b13d52f42a6f2ffb967072a7d5cbc1fd4f095cd
smitjl13 <jason.l.smith@vanderbilt.edu>
2024-10-14 12:35:57 -0500

    Saving Day 16 progress

--- Day 16 - Turtle Graphics/main.py
+++ Day 16 - Turtle Graphics/main.py
@@ -1,3 +1,24 @@
-from turtle import Turtle
-timmy = Turtle()
-print(timmy)
+# from turtle import Turtle, Screen
+# timmy = Turtle()
+# #print(timmy)
+# timmy.shape("turtle")
+# timmy.color("DarkSeaGreen4")
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+
+
+from prettytable import PrettyTable
+table = PrettyTable()
+
+pokemon = ['Pikachu', 'Squirtle', 'Charmader']
+pokemon_type = ['Electric', 'Water', 'Fire']
+
+table.add_column("Pokemon Name", pokemon)
+table.add_column("Type", pokemon_type)
+table.align = 'l'
+
+print(table)
